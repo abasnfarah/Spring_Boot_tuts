401f72638bf33d395326266685747a4e390e5d30
Abas Farah <abasnfarah@gmail.com>
2020-06-01 19:26:54 -0500

    API Post Delete endpoint updated

--- SpringBoot_Conference-demo/src/main/java/com/pluralsight/SpringBoot_Conferencedemo/controllers/SessionsController.java
+++ SpringBoot_Conference-demo/src/main/java/com/pluralsight/SpringBoot_Conferencedemo/controllers/SessionsController.java
@@ -2,6 +2,7 @@ package com.pluralsight.SpringBoot_Conferencedemo.controllers;
 
 import com.pluralsight.SpringBoot_Conferencedemo.models.Session;
 import com.pluralsight.SpringBoot_Conferencedemo.repositories.SessionRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,6 +28,22 @@ public class SessionsController {
     @PostMapping
     public Session create(@RequestBody final Session session){
         return sessionRepository.saveAndFlush(session);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        // Also need to check for child records
+        sessionRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Session update(@PathVariable Long id, @RequestBody Session session){
+        // Becuase it is a put all values are expected to be passed in.
+        // A patch would only need the parts to update
+        //TODO: add validation that all atributes are passed in, otherwise return a 400 bad payload
+        Session existingSession = sessionRepository.getOne(id);
+        BeanUtils.copyProperties(session,existingSession,"session_id");
+        return sessionRepository.saveAndFlush(existingSession);
     }
 
 }
--- SpringBoot_Conference-demo/src/main/java/com/pluralsight/SpringBoot_Conferencedemo/controllers/SpeakersController.java
+++ SpringBoot_Conference-demo/src/main/java/com/pluralsight/SpringBoot_Conferencedemo/controllers/SpeakersController.java
@@ -2,11 +2,9 @@ package com.pluralsight.SpringBoot_Conferencedemo.controllers;
 
 import com.pluralsight.SpringBoot_Conferencedemo.models.Speaker;
 import com.pluralsight.SpringBoot_Conferencedemo.repositories.SpeakerRepository;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,5 +24,27 @@ public class SpeakersController {
     public Speaker get(@PathVariable long id) {
         return speakerRepository.getOne(id);
     }
+
+    @PostMapping
+    public Speaker create(@RequestBody final Speaker speaker){
+        return speakerRepository.saveAndFlush(speaker);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.DELETE)
+    public void delete(@PathVariable Long id){
+        // Also need to check for child records
+        speakerRepository.deleteById(id);
+    }
+
+    @RequestMapping(value = "{id}", method = RequestMethod.PUT)
+    public Speaker update(@PathVariable Long id, @RequestBody Speaker speaker){
+        // Becuase it is a put all values are expected to be passed in.
+        // A patch would only need the parts to update
+        //TODO: add validation that all atributes are passed in, otherwise return a 400 bad payload
+        Speaker existingSpeaker = speakerRepository.getOne(id);
+        BeanUtils.copyProperties(speaker,existingSpeaker,"speaker_id");
+        return speakerRepository.saveAndFlush(existingSpeaker);
+    }
+
 
 }
